5ec5e41f73b1c4ee61075ec111a8a19caac34cbf
Pritam <pritam7796419792@gmail.com>
2024-10-03 15:16:15 +0530

    dd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+
 import express from "express";
-import "dotenv/config";
 import database from "./src/database/mongoDb.js";
 import adminRote from "./src/routes/admin/adminRote.js";
 import profile from "./src/routes/admin/profile.js";
-import expenseRoute from "./src/routes/expenses/expenseRoute.js";
-import moneyAddrerRoute from "./src/routes/money/moneyAddrerRoute.js";
-import historyRoute from "./src/routes/history/historyRoute.js";
+import expenseRoute from './src/routes/expenses/expenseRoute.js'
+import moneyAddrerRoute from './src/routes/money/moneyAddrerRoute.js'
+import historyRoute from './src/routes/history/historyRoute.js'
 import cors from "cors";
 
 // Import routes
@@ -28,6 +28,5 @@ app.use("/expense", expenseRoute);
 app.use("/money", moneyAddrerRoute);
 app.use("/history", historyRoute);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const port = 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- src/database/mongoDb.js
+++ src/database/mongoDb.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-import "dotenv/config";
-
 const Database = () => {
   try {
-    mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(
+      "mongodb+srv://pritam7796419792:ONKdNVSGrnQUVBzX@fiancemanagment.uin8m.mongodb.net/finance?retryWrites=true&w=majority&appName=FianceManagment"
+    );
     console.log("Database connected successfully");
   } catch (error) {}
 };
 export default Database;
+ 
